840022e2b64770d57e99c1d82c74245e8ca0ef28
unknown <kingj2@USSAKINGJ2L1C.corp.emc.com>
2013-02-27 15:58:55 -0500

    Initial Battery Monitor Commit

--- BatteryMonitor/BatteryMonitor.cpp
+++ BatteryMonitor/BatteryMonitor.cpp
@@ -1,13 +1,20 @@
 /*
+<<<<<<< HEAD
   BatteryMonitor.cpp - Library for monitoring battery voltage.
   Created by James King, February 27, 2013
   
   This class uses a voltage divider shunt on an Arduino analog
   pin to measure a battery voltage.
+=======
+  BatteryMonitor.h - Library for monitoring battery voltage.
+  Created by James King, February 27, 2013
+  Released into the public domain.
+>>>>>>> Initial Battery Monitor Commit
 */
 
 #include "BatteryMonitor.h"
 
+<<<<<<< HEAD
 //constructor function
 BatteryMonitor::BatteryMonitor(byte pin, float r1, float r2)
 {
@@ -43,13 +50,57 @@ float BatteryMonitor::read()
 }
 
 //set calibration values
+=======
+BatteryMonitor::BatteryMonitor(byte pin, float r1, float r2)
+{
+	pinMode(pin,INPUT);
+	batteryPin = pin;
+	batteryVoltage = 0;
+	ratio = r2/(r1+r2);
+	adjustValue = 0;
+	
+}
+
+BatteryMonitor::~BatteryMonitor()
+{
+	batteryVoltage = 0;
+	ratio = 0;
+	R1 = 0;
+	R2 = 0;
+	adjustValue = 0;
+}
+
+float BatteryMonitor::read()
+{
+	batteryVoltage = analogRead(batteryPin);
+	batteryVoltage = batteryVoltage/1023*5;
+	batteryVoltage = batteryVoltgage/ratio;
+	batteryVoltage = batteryVoltage + adjustValue;
+	return batteryVoltage;
+}
+
+>>>>>>> Initial Battery Monitor Commit
 void BatteryMonitor::calibrate(int calibrationValue)
 {
 	adjustValue = calibrationValue;
 }
 
+<<<<<<< HEAD
 //return the last read value instead of doing a new read
 float BatteryMonitor::lastValue()
 {
 	return batteryVoltage;
 }
+=======
+float BatteryMonitor::lastValue()
+{
+	return batteryVoltage;
+}
+  private:
+	float batteryVoltage;
+    byte batteryPin;
+	float ratio;
+	int adjustValue;
+};
+
+>>>>>>> Initial Battery Monitor Commit
--- BatteryMonitor/BatteryMonitor.h
+++ BatteryMonitor/BatteryMonitor.h
@@ -1,9 +1,13 @@
 /*
   BatteryMonitor.h - Library for monitoring battery voltage.
   Created by James King, February 27, 2013
+<<<<<<< HEAD
   
   This class uses a voltage divider shunt on an Arduino analog
   pin to measure a battery voltage.
+=======
+  Released into the public domain.
+>>>>>>> Initial Battery Monitor Commit
 */
 
 #ifndef BatteryMonitor_h
@@ -11,7 +15,11 @@
 
 #include "WProgram.h"
 
+<<<<<<< HEAD
 class BatteryMonitor
+=======
+class BatterMonitor
+>>>>>>> Initial Battery Monitor Commit
 {
   public:
     BatteryMonitor(byte pin, float r1, float r2);
