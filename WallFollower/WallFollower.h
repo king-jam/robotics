c60223cdef2caf0af5ef0213d4a6d06a48a4b0b3
Steve MacKinnon <mackinnon.st@gmail.com>
2013-04-05 11:30:51 -0400

    Adding special cases for acute turn. Not fully functional yet.

---
+++ WallFollower/WallFollower.h
@@ -0,0 +1,20 @@
+/*
+  WallFollower.h - Main wall follower robot code.
+  Created by Steve MacKinnon, March 19, 2013
+*/
+
+#ifndef WallFollower_h
+#define WallFollower_h
+
+#include "DCMotor.h"
+#include "BatteryMonitor.h"
+
+class WallFollower {
+public:
+	WallFollower();
+	~WallFollower();
+
+
+}
+
+#endif
